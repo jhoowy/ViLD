Implement proposal embedding calculation & Fix vild head

CLIP_embedder/img2emb.py
@@ -11,166 +11,20 @@ import clip
 from PIL import Image
 import argparse
 
-BASE_CLASSES = (
-    'aerosol_can', 'air_conditioner', 'airplane', 'alarm_clock', 'alcohol', 
-    'alligator', 'almond', 'ambulance', 'amplifier', 'anklet', 'antenna', 
-    'apple', 'apron', 'aquarium', 'armband', 'armchair', 'artichoke', 
-    'trash_can', 'ashtray', 'asparagus', 'atomizer', 'avocado', 'award', 
-    'awning', 'baby_buggy', 'basketball_backboard', 'backpack', 'handbag', 
-    'suitcase', 'bagel', 'ball', 'balloon', 'bamboo', 'banana', 'Band_Aid', 
-    'bandage', 'bandanna', 'banner', 'barrel', 'barrette', 'barrow', 'baseball_base', 
-    'baseball', 'baseball_bat', 'baseball_cap', 'baseball_glove', 'basket', 
-    'basketball', 'bat_(animal)', 'bath_mat', 'bath_towel', 'bathrobe', 
-    'bathtub', 'battery', 'bead', 'bean_curd', 'beanbag', 'beanie', 'bear', 
-    'bed', 'bedspread', 'cow', 'beef_(food)', 'beer_bottle', 'beer_can', 
-    'bell', 'bell_pepper', 'belt', 'belt_buckle', 'bench', 'beret', 'bib', 
-    'bicycle', 'visor', 'billboard', 'binder', 'binoculars', 'bird', 'birdfeeder', 
-    'birdbath', 'birdcage', 'birdhouse', 'birthday_cake', 'black_sheep', 
-    'blackberry', 'blackboard', 'blanket', 'blazer', 'blender', 'blinker', 
-    'blouse', 'blueberry', 'boat', 'bobbin', 'bobby_pin', 'boiled_egg', 
-    'deadbolt', 'bolt', 'book', 'bookcase', 'booklet', 'boot', 'bottle', 
-    'bottle_opener', 'bouquet', 'bow_(decorative_ribbons)', 'bow-tie', 
-    'bowl', 'bowler_hat', 'box', 'suspenders', 'bracelet', 'brassiere', 
-    'bread-bin', 'bread', 'bridal_gown', 'briefcase', 'broccoli', 'broom', 
-    'brownie', 'brussels_sprouts', 'bucket', 'bull', 'bulldog', 'bullet_train', 
-    'bulletin_board', 'bullhorn', 'bun', 'bunk_bed', 'buoy', 'bus_(vehicle)', 
-    'business_card', 'butter', 'butterfly', 'button', 'cab_(taxi)', 'cabin_car', 
-    'cabinet', 'cake', 'calculator', 'calendar', 'calf', 'camcorder', 
-    'camel', 'camera', 'camera_lens', 'camper_(vehicle)', 'can', 'can_opener', 
-    'candle', 'candle_holder', 'candy_cane', 'walking_cane', 'canister', 
-    'canoe', 'cantaloup', 'cap_(headwear)', 'bottle_cap', 'cape', 'cappuccino', 
-    'car_(automobile)', 'railcar_(part_of_a_train)', 'identity_card', 
-    'card', 'cardigan', 'horse_carriage', 'carrot', 'tote_bag', 'cart', 
-    'carton', 'cash_register', 'cast', 'cat', 'cauliflower', 'cayenne_(spice)', 
-    'CD_player', 'celery', 'cellular_telephone', 'chair', 'chandelier', 
-    'cherry', 'chicken_(animal)', 'chickpea', 'chili_(vegetable)', 'crisp_(potato_chip)', 
-    'chocolate_bar', 'chocolate_cake', 'choker', 'chopping_board', 'chopstick', 
-    'Christmas_tree', 'slide', 'cigarette', 'cigarette_case', 'cistern', 
-    'clasp', 'cleansing_agent', 'clip', 'clipboard', 'clock', 'clock_tower', 
-    'clothes_hamper', 'clothespin', 'coaster', 'coat', 'coat_hanger', 
-    'coatrack', 'cock', 'coconut', 'coffee_maker', 'coffee_table', 'coffeepot', 
-    'coin', 'colander', 'coleslaw', 'pacifier', 'computer_keyboard', 'condiment', 
-    'cone', 'control', 'cookie', 'cooler_(for_food)', 'cork_(bottle_plug)', 
-    'corkscrew', 'edible_corn', 'cornet', 'cornice', 'corset', 'costume', 
-    'cowbell', 'cowboy_hat', 'crab_(animal)', 'cracker', 'crate', 'crayon', 
-    'crescent_roll', 'crib', 'crock_pot', 'crossbar', 'crow', 'crown', 
-    'crucifix', 'cruise_ship', 'police_cruiser', 'crumb', 'crutch', 'cub_(animal)', 
-    'cube', 'cucumber', 'cufflink', 'cup', 'trophy_cup', 'cupboard', 'cupcake', 
-    'curtain', 'cushion', 'dartboard', 'deck_chair', 'deer', 'dental_floss', 
-    'desk', 'diaper', 'dining_table', 'dish', 'dish_antenna', 'dishrag', 
-    'dishtowel', 'dishwasher', 'dispenser', 'Dixie_cup', 'dog', 'dog_collar', 
-    'doll', 'dolphin', 'domestic_ass', 'doorknob', 'doormat', 'doughnut', 
-    'drawer', 'underdrawers', 'dress', 'dress_hat', 'dress_suit', 'dresser', 
-    'drill', 'drum_(musical_instrument)', 'duck', 'duckling', 'duct_tape', 
-    'duffel_bag', 'dumpster', 'eagle', 'earphone', 'earring', 'easel', 
-    'egg', 'egg_yolk', 'eggbeater', 'eggplant', 'refrigerator', 'elephant', 
-    'elk', 'envelope', 'eraser', 'fan', 'faucet', 'Ferris_wheel', 'ferry', 
-    'fighter_jet', 'figurine', 'file_cabinet', 'fire_alarm', 'fire_engine', 
-    'fire_extinguisher', 'fire_hose', 'fireplace', 'fireplug', 'fish', 
-    'fish_(food)', 'fishing_rod', 'flag', 'flagpole', 'flamingo', 'flannel', 
-    'flap', 'flashlight', 'flip-flop_(sandal)', 'flipper_(footwear)', 
-    'flower_arrangement', 'flute_glass', 'foal', 'folding_chair', 'food_processor', 
-    'football_(American)', 'footstool', 'fork', 'forklift', 'freight_car', 
-    'French_toast', 'freshener', 'frisbee', 'frog', 'fruit_juice', 'frying_pan', 
-    'garbage_truck', 'garden_hose', 'gargle', 'garlic', 'gazelle', 'gelatin', 
-    'giant_panda', 'gift_wrap', 'ginger', 'giraffe', 'cincture', 'glass_(drink_container)', 
-    'globe', 'glove', 'goat', 'goggles', 'golf_club', 'golfcart', 'goose', 
-    'grape', 'grater', 'gravestone', 'green_bean', 'green_onion', 'grill', 
-    'grizzly', 'grocery_bag', 'guitar', 'gull', 'gun', 'hairbrush', 'hairnet', 
-    'hairpin', 'ham', 'hamburger', 'hammer', 'hammock', 'hamster', 'hair_dryer', 
-    'hand_towel', 'handcart', 'handkerchief', 'handle', 'hat', 'veil', 
-    'headband', 'headboard', 'headlight', 'headscarf', 'headstall_(for_horses)', 
-    'heart', 'heater', 'helicopter', 'helmet', 'highchair', 'hinge', 'hog', 
-    'home_plate_(baseball)', 'honey', 'fume_hood', 'hook', 'horse', 'hose', 
-    'hot_sauce', 'hummingbird', 'polar_bear', 'icecream', 'ice_maker', 
-    'igniter', 'iPod', 'iron_(for_clothing)', 'ironing_board', 'jacket', 
-    'jam', 'jar', 'jean', 'jeep', 'jersey', 'jet_plane', 'jewelry', 'jumpsuit', 
-    'kayak', 'kettle', 'key', 'kilt', 'kimono', 'kitchen_sink', 'kite', 
-    'kitten', 'kiwi_fruit', 'knee_pad', 'knife', 'knob', 'ladder', 'ladle', 
-    'ladybug', 'lamb_(animal)', 'lamp', 'lamppost', 'lampshade', 'lantern', 
-    'lanyard', 'laptop_computer', 'latch', 'legging_(clothing)', 'Lego', 
-    'lemon', 'lettuce', 'license_plate', 'life_buoy', 'life_jacket', 'lightbulb', 
-    'lime', 'lion', 'lip_balm', 'lizard', 'log', 'lollipop', 'speaker_(stero_equipment)', 
-    'loveseat', 'magazine', 'magnet', 'mail_slot', 'mailbox_(at_home)', 
-    'mandarin_orange', 'manger', 'manhole', 'map', 'marker', 'mashed_potato', 
-    'mask', 'mast', 'mat_(gym_equipment)', 'mattress', 'measuring_cup', 
-    'measuring_stick', 'meatball', 'medicine', 'melon', 'microphone', 
-    'microwave_oven', 'milk', 'minivan', 'mirror', 'mitten', 'mixer_(kitchen_tool)', 
-    'money', 'monitor_(computer_equipment) computer_monitor', 'monkey', 
-    'motor', 'motor_scooter', 'motorcycle', 'mound_(baseball)', 'mouse_(computer_equipment)', 
-    'mousepad', 'muffin', 'mug', 'mushroom', 'musical_instrument', 'napkin', 
-    'necklace', 'necktie', 'needle', 'nest', 'newspaper', 'newsstand', 
-    'nightshirt', 'noseband_(for_animals)', 'notebook', 'notepad', 'nut', 
-    'oar', 'oil_lamp', 'olive_oil', 'onion', 'orange_(fruit)', 'orange_juice', 
-    'ostrich', 'ottoman', 'oven', 'overalls_(clothing)', 'owl', 'packet', 
-    'pad', 'paddle', 'padlock', 'paintbrush', 'painting', 'pajamas', 'palette', 
-    'pan_(for_cooking)', 'pancake', 'paper_plate', 'paper_towel', 'parachute', 
-    'parakeet', 'parasail_(sports)', 'parasol', 'parka', 'parking_meter', 
-    'parrot', 'passenger_car_(part_of_a_train)', 'passport', 'pastry', 
-    'pea_(food)', 'peach', 'peanut_butter', 'pear', 'peeler_(tool_for_fruit_and_vegetables)', 
-    'pelican', 'pen', 'pencil', 'penguin', 'pepper', 'pepper_mill', 'perfume', 
-    'person', 'pet', 'pew_(church_bench)', 'phonograph_record', 'piano', 
-    'pickle', 'pickup_truck', 'pie', 'pigeon', 'pillow', 'pineapple', 
-    'pinecone', 'pipe', 'pita_(bread)', 'pitcher_(vessel_for_liquid)', 
-    'pizza', 'place_mat', 'plate', 'platter', 'pliers', 'pocketknife', 
-    'poker_(fire_stirring_tool)', 'pole', 'polo_shirt', 'pony', 'pop_(soda)', 
-    'postbox_(public)', 'postcard', 'poster', 'pot', 'flowerpot', 'potato', 
-    'potholder', 'pottery', 'pouch', 'power_shovel', 'prawn', 'pretzel', 
-    'printer', 'projectile_(weapon)', 'projector', 'propeller', 'pumpkin', 
-    'puppy', 'quilt', 'rabbit', 'racket', 'radiator', 'radio_receiver', 
-    'radish', 'raft', 'raincoat', 'ram_(animal)', 'raspberry', 'razorblade', 
-    'reamer_(juicer)', 'rearview_mirror', 'receipt', 'recliner', 'record_player', 
-    'reflector', 'remote_control', 'rhinoceros', 'rifle', 'ring', 'robe', 
-    'rocking_chair', 'rolling_pin', 'router_(computer_equipment)', 'rubber_band', 
-    'runner_(carpet)', 'plastic_bag', 'saddle_(on_an_animal)', 'saddle_blanket', 
-    'saddlebag', 'sail', 'salad', 'salami', 'salmon_(fish)', 'salsa', 
-    'saltshaker', 'sandal_(type_of_shoe)', 'sandwich', 'saucer', 'sausage', 
-    'scale_(measuring_instrument)', 'scarf', 'school_bus', 'scissors', 
-    'scoreboard', 'screwdriver', 'scrubbing_brush', 'sculpture', 'seabird', 
-    'seahorse', 'seashell', 'sewing_machine', 'shaker', 'shampoo', 'shark', 
-    'shaving_cream', 'sheep', 'shield', 'shirt', 'shoe', 'shopping_bag', 
-    'shopping_cart', 'short_pants', 'shoulder_bag', 'shovel', 'shower_head', 
-    'shower_curtain', 'signboard', 'silo', 'sink', 'skateboard', 'skewer', 
-    'ski', 'ski_boot', 'ski_parka', 'ski_pole', 'skirt', 'sled', 'sleeping_bag', 
-    'slipper_(footwear)', 'snowboard', 'snowman', 'snowmobile', 'soap', 
-    'soccer_ball', 'sock', 'sofa', 'solar_array', 'soup', 'soupspoon', 
-    'sour_cream', 'spatula', 'spectacles', 'spice_rack', 'spider', 'sponge', 
-    'spoon', 'sportswear', 'spotlight', 'squirrel', 'stapler_(stapling_machine)', 
-    'starfish', 'statue_(sculpture)', 'steak_(food)', 'steering_wheel', 
-    'step_stool', 'stereo_(sound_system)', 'stirrup', 'stool', 'stop_sign', 
-    'brake_light', 'stove', 'strainer', 'strap', 'straw_(for_drinking)', 
-    'strawberry', 'street_sign', 'streetlight', 'suit_(clothing)', 'sunflower', 
-    'sunglasses', 'sunhat', 'surfboard', 'sushi', 'mop', 'sweat_pants', 
-    'sweatband', 'sweater', 'sweatshirt', 'sweet_potato', 'swimsuit', 
-    'sword', 'table', 'table_lamp', 'tablecloth', 'tag', 'taillight', 
-    'tank_(storage_vessel)', 'tank_top_(clothing)', 'tape_(sticky_cloth_or_paper)', 
-    'tape_measure', 'tapestry', 'tarp', 'tartan', 'tassel', 'tea_bag', 
-    'teacup', 'teakettle', 'teapot', 'teddy_bear', 'telephone', 'telephone_booth', 
-    'telephone_pole', 'television_camera', 'television_set', 'tennis_ball', 
-    'tennis_racket', 'thermometer', 'thermos_bottle', 'thermostat', 'thread', 
-    'thumbtack', 'tiara', 'tiger', 'tights_(clothing)', 'timer', 'tinfoil', 
-    'tinsel', 'tissue_paper', 'toast_(food)', 'toaster', 'toaster_oven', 
-    'toilet', 'toilet_tissue', 'tomato', 'tongs', 'toolbox', 'toothbrush', 
-    'toothpaste', 'toothpick', 'cover', 'tortilla', 'tow_truck', 'towel', 
-    'towel_rack', 'toy', 'tractor_(farm_equipment)', 'traffic_light', 
-    'dirt_bike', 'trailer_truck', 'train_(railroad_vehicle)', 'tray', 
-    'tricycle', 'tripod', 'trousers', 'truck', 'trunk', 'turban', 'turkey_(food)', 
-    'turtle', 'turtleneck_(clothing)', 'typewriter', 'umbrella', 'underwear', 
-    'urinal', 'urn', 'vacuum_cleaner', 'vase', 'vending_machine', 'vent', 
-    'vest', 'videotape', 'volleyball', 'waffle', 'wagon', 'wagon_wheel', 
-    'walking_stick', 'wall_clock', 'wall_socket', 'wallet', 'automatic_washer', 
-    'watch', 'water_bottle', 'water_cooler', 'water_faucet', 'water_jug', 
-    'water_scooter', 'water_ski', 'water_tower', 'watering_can', 'watermelon', 
-    'weathervane', 'webcam', 'wedding_cake', 'wedding_ring', 'wet_suit', 
-    'wheel', 'wheelchair', 'whipped_cream', 'whistle', 'wig', 'wind_chime', 
-    'windmill', 'window_box_(for_plants)', 'windshield_wiper', 'windsock', 
-    'wine_bottle', 'wine_bucket', 'wineglass', 'blinder_(for_horses)', 
-    'wok', 'wooden_spoon', 'wreath', 'wrench', 'wristband', 'wristlet', 
-    'yacht', 'yogurt', 'yoke_(animal_equipment)', 'zebra', 'zucchini')
+from mmdet.apis import init_detector, inference_detector
+
+class ImageCLIP(torch.nn.Module):
+    def __init__(self, model) :
+        super(ImageCLIP, self).__init__()
+        self.model = model
+        
+    def forward(self,image):
+        return self.model.encode_image(image)
 
 parser = argparse.ArgumentParser(description='Create image embedding for ViLD')
 parser.add_argument('--data_root', default='/data/project/rw/lvis_v1', type=str)
-parser.add_argument('--save_dir', default='img_embeddings', type=str)
+parser.add_argument('--proposal_dir', default='proposals', type=str)
+parser.add_argument('--save_dir', default='proposal_embeddings', type=str)
 args = parser.parse_args()
 
 data_root = args.data_root
@@ -179,16 +33,14 @@ save_dir = osp.join(data_root, args.save_dir)
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-B/32", device=device)
+model_image = torch.nn.DataParallel(ImageCLIP(model))
 
 os.makedirs(osp.join(save_dir, 'train2017'), exist_ok=True)
 os.makedirs(osp.join(save_dir, 'val2017'), exist_ok=True)
 
 coco = LVIS(ann_file)
 img_ids = coco.get_img_ids()
-base_cat_ids = []
-for i in coco.cats:
-    if coco.cats[i]['name'] in BASE_CLASSES:
-        base_cat_ids.append(coco.cats[i]['id'])
+img_ids = img_ids
 
 for i in tqdm(img_ids):
     img_info = coco.load_imgs([i])[0]
@@ -197,41 +49,58 @@ for i in tqdm(img_ids):
     img = Image.open(osp.join(data_root, filename))
     outname = '.'.join((filename.split('.')[0], 'pickle'))
 
-    ann_ids = coco.get_ann_ids(img_ids=[i], cat_ids=base_cat_ids)
-    ann_info = coco.load_anns(ann_ids)
-    embeddings = {}
-    for ann in ann_info:
-        ann_id = ann['id']
+    p_filename = '.'.join((filename.split('.')[0] + '_p', 'pickle'))
+    proposal_path = osp.join(data_root, args.proposal_dir, p_filename)
+    with open(proposal_path, 'rb') as f:
+        proposals = pickle.load(f)
 
-        if ann.get('ignore', False):
-            continue
-
-        x1, y1, w, h = ann['bbox']
-        inter_w = max(0, min(x1 + w, img_info['width']) - max(x1, 0))
-        inter_h = max(0, min(y1 + h, img_info['height']) - max(y1, 0))
-        if inter_w * inter_h == 0:
-            continue
-        if ann['area'] <= 0 or w < 1 or h < 1:
+    results = {}
+    embeddings = []
+    embed_bboxes = []
+    embed_scores = []
+    batch = []
+    batch_l = []
+    for p in proposals:
+        x1, y1, x2, y2, s = p.numpy()
+        w = x2 - x1
+        h = y2 - y1
+        
+        if w < 1 or h < 1:
             continue
 
         # x1.5 crop
-        x2 = max(0, x1 - w//2)
-        y2 = max(0, y1 - h//2)
-        xr2 = min(x1 + w + w//2, img_info['width'])
-        yr2 = min(y1 + h + h//2, img_info['height'])
+        lx1 = max(0, x1 - w//2)
+        ly1 = max(0, y1 - h//2)
+        lx2 = min(x1 + w + w//2, img_info['width'])
+        ly2 = min(y1 + h + h//2, img_info['height'])
 
-        im_crop = img.crop((x1, y1, x1 + w, y1 + h))
+        im_crop = img.crop((x1, y1, x2, y2))
         im_crop = preprocess(im_crop).unsqueeze(0).to(device)
-        im_crop2 = img.crop((x2, y2, xr2, yr2))
-        im_crop2 = preprocess(im_crop2).unsqueeze(0).to(device)
-        with torch.no_grad():
-            # Fix later to batch inference
-            im_embed = model.encode_image(im_crop)
-            im_embed2 = model.encode_image(im_crop2)
-            im_embed += im_embed2
-            im_embed /= im_embed.norm(dim=-1, keepdim=True)
+        im_crop_l = img.crop((lx1, ly1, lx2, ly2))
+        im_crop_l = preprocess(im_crop_l).unsqueeze(0).to(device)
 
-        embeddings[ann_id] = im_embed.cpu().numpy()
+        batch.append(im_crop)
+        batch_l.append(im_crop_l)
+        embed_bboxes.append(p[:4])
+        embed_scores.append(p[4])
+
+    with torch.no_grad():
+        batch_size = len(batch)
+        input = torch.cat(batch + batch_l)
+        im_embed = model_image(input)
+        im_embed = im_embed[:batch_size] + im_embed[batch_size:]
+        im_embed /= im_embed.norm(dim=-1, keepdim=True)
+    embeddings.append(im_embed)
+    batch = []
+    batch_l = []
+    
+    embeddings = torch.cat(embeddings).cpu().numpy()
+    embed_bboxes = torch.stack(embed_bboxes).cpu().numpy()
+    embed_scores = torch.stack(embed_scores).cpu().numpy()
+
+    results['img_embeds'] = embeddings
+    results['bboxes'] = embed_bboxes
+    results['scores'] = embed_scores
     
     with open(osp.join(save_dir, outname), 'wb') as out:
-        pickle.dump(embeddings, out, pickle.HIGHEST_PROTOCOL)
+        pickle.dump(results, out, pickle.HIGHEST_PROTOCOL)

mmdet/datasets/pipelines/load_embedding.py
@@ -12,7 +12,8 @@ from mmdet.datasets import PIPELINES
 class LoadEmbeddingFromFile:
     """Load embeddings from file.
 
-    Required keys are "emb_prefix" and "emb_filename". Added key is "gt_embeds".
+    Required keys are "emb_prefix" and "emb_filename". Added key is "gt_embeds", 
+    "gt_embed_bboxes", and "gt_embed_weights" (optional).
     
     Args:
         with_score (bool) : Whether to parse and load the embedding score.
@@ -37,7 +38,7 @@ class LoadEmbeddingFromFile:
             with open(self.ann_file, 'r') as f:
                 weight_data = json.load(f)
             for k, v in weight_data.items():
-                self.embed_weights[int(k)] = v
+                self.embed_weights[k] = v
 
     def __call__(self, results):
         if self.file_client is None:
@@ -50,18 +51,24 @@ class LoadEmbeddingFromFile:
             filename = results['emb_filename']
 
         with open(filename, 'rb') as f:
-            img_embeds = pickle.load(f)
+            data = pickle.load(f)
 
-        ann_ids = results['ann_info']['ann_ids']
-        gt_embeds = [img_embeds[id].astype(np.float32) for id in ann_ids]
+        gt_embeds = data['img_embeds']
+        gt_embed_bboxes = data['bboxes']
+
         if len(gt_embeds) > 0:
-            gt_embeds = np.concatenate(gt_embeds)
+            gt_embeds = np.concatenate(gt_embeds).astype(np.float32)
+            gt_embed_bboxes = np.concatenate(gt_embed_bboxes).astype(np.float32)
         else:
-            gt_embeds = np.empty((0, 512)).astype(np.float32)
+            gt_embeds = np.empty((0.512)).astype(np.float32)
+            gt_embed_bboxes = np.empty((0, 4)).astype(np.float32)
+
         results['gt_embeds'] = gt_embeds
+        results['gt_embed_bboxes'] = gt_embed_bboxes
+        results['bbox_fields'].append('gt_embed_bboxes')
 
         if self.with_score:
-            gt_embed_weights = np.array([self.embed_weights[id] for id in ann_ids])
+            gt_embed_weights = np.array(self.embed_weights[results['emb_filename']])
             results['gt_embed_weights'] = gt_embed_weights.astype(np.float32)
         return results
 

mmdet/models/roi_heads/bbox_heads/vild_head.py
@@ -15,7 +15,7 @@ from mmdet.models.utils import build_linear_layer
 
 
 @HEADS.register_module()
-class EmbeddingBBoxHead(BBoxHead):
+class ViLDBBoxHead(BBoxHead):
     r"""Classification based on pre-defined text embedding.
 
     Args:
@@ -26,11 +26,11 @@ class EmbeddingBBoxHead(BBoxHead):
                  base_emb_path,
                  novel_emb_path=None,
                  num_shared_convs=0,
-                 num_shared_fcs=0,
+                 num_shared_fcs=2,
                  num_cls_convs=0,
-                 num_cls_fcs=2,
+                 num_cls_fcs=0,
                  num_reg_convs=0,
-                 num_reg_fcs=2,
+                 num_reg_fcs=0,
                  conv_out_channels=256,
                  fc_out_channels=1024,
                  emb_channels=512,
@@ -43,7 +43,7 @@ class EmbeddingBBoxHead(BBoxHead):
                  init_cfg=None,
                  *args,
                  **kwargs):
-        super(EmbeddingBBoxHead, self).__init__(
+        super(ViLDBBoxHead, self).__init__(
             *args, init_cfg=init_cfg, **kwargs)
         assert (num_shared_convs + num_shared_fcs + num_cls_convs +
                 num_cls_fcs + num_reg_convs + num_reg_fcs > 0)
@@ -179,8 +179,21 @@ class EmbeddingBBoxHead(BBoxHead):
         return branch_convs, branch_fcs, last_layer_dim
 
     def forward(self, x):
+        region_embed = self.forward_embed(x)
         cls_emb = torch.cat((self.text_embeddings, self.bg_embedding), dim=0)
 
+        self.logit_scale.data.clamp_(-np.log(100), np.log(100))
+        logit_scale = self.logit_scale.exp()
+
+        if self.with_cls:
+            cls_score = logit_scale * region_embed @ cls_emb.t()
+        else:
+            cls_score = None
+        bbox_pred = self.fc_reg(x_reg) if self.with_reg else None
+
+        return cls_score, bbox_pred
+
+    def forward_embed(self, x):
         # shared part
         if self.num_shared_convs > 0:
             for conv in self.shared_convs:
@@ -215,147 +228,7 @@ class EmbeddingBBoxHead(BBoxHead):
             x_reg = self.relu(fc(x_reg))
 
         region_embed = F.normalize(self.fc_proj(x_cls), dim=-1)
-        self.logit_scale.data.clamp_(-np.log(100), np.log(100))
-        logit_scale = self.logit_scale.exp()
-        if self.with_cls:
-            cls_score = logit_scale * region_embed @ cls_emb.t()
-        else:
-            cls_score = None
-        bbox_pred = self.fc_reg(x_reg) if self.with_reg else None
-        return cls_score, bbox_pred, region_embed
-
-    def _get_target_single(self, pos_bboxes, neg_bboxes, pos_gt_bboxes,
-                           pos_gt_labels, pos_gt_embeds, pos_gt_embed_weights,
-                           pos_is_gt, cfg):
-        """Calculate the ground truth for proposals in the single image
-        according to the sampling results.
-
-        Args:
-            pos_bboxes (Tensor): Contains all the positive boxes,
-                has shape (num_pos, 4), the last dimension 4
-                represents [tl_x, tl_y, br_x, br_y].
-            neg_bboxes (Tensor): Contains all the negative boxes,
-                has shape (num_neg, 4), the last dimension 4
-                represents [tl_x, tl_y, br_x, br_y].
-            pos_gt_bboxes (Tensor): Contains gt_boxes for
-                all positive samples, has shape (num_pos, 4),
-                the last dimension 4
-                represents [tl_x, tl_y, br_x, br_y].
-            pos_gt_labels (Tensor): Contains gt_labels for
-                all positive samples, has shape (num_pos, ).
-            pos_gt_embeds (Tensor): Contains gt_embeds for
-                all positive samples, has shape (num_pos, emb_channels).
-            pos_gt_embed_weights (Tensor): Contains weights of gt_embed
-                for all positive samples, has shape (num_pos, ).
-            cfg (obj:`ConfigDict`): `train_cfg` of R-CNN.
-
-        Returns:
-            Tuple[Tensor]: Ground truth for proposals
-            in a single image. Containing the following Tensors:
-
-                - labels(Tensor): Gt_labels for all proposals, has
-                  shape (num_proposals,).
-                - label_weights(Tensor): Labels_weights for all
-                  proposals, has shape (num_proposals,).
-                - bbox_targets(Tensor):Regression target for all
-                  proposals, has shape (num_proposals, 4), the
-                  last dimension 4 represents [tl_x, tl_y, br_x, br_y].
-                - bbox_weights(Tensor):Regression weights for all
-                  proposals, has shape (num_proposals, 4).
-                - embeds(Tensor):Gt_embeds for all proposals, has
-                  shape (num_proposals, emb_channels).
-                - embed_weights(Tensor):Loss weight for all gt embeds, has
-                  shape (num_proposals, emb_channels).
-        """
-        num_pos = pos_bboxes.size(0)
-        num_neg = neg_bboxes.size(0)
-        num_samples = num_pos + num_neg
-
-        # original implementation uses new_zeros since BG are set to be 0
-        # now use empty & fill because BG cat_id = num_classes,
-        # FG cat_id = [0, num_classes-1]
-        labels = pos_bboxes.new_full((num_samples, ),
-                                     self.num_classes,
-                                     dtype=torch.long)
-        embeds = pos_bboxes.new_zeros(num_samples, self.emb_channels)
-        label_weights = pos_bboxes.new_zeros(num_samples)
-        bbox_targets = pos_bboxes.new_zeros(num_samples, 4)
-        bbox_weights = pos_bboxes.new_zeros(num_samples, 4)
-        embed_weights = pos_bboxes.new_zeros(num_samples, self.emb_channels)
-        if num_pos > 0:
-            labels[:num_pos] = pos_gt_labels
-            embeds[:num_pos] = pos_gt_embeds
-            embed_weights[:num_pos, :] = 1
-            if len(pos_gt_embed_weights) > 0:
-                embed_weights[:num_pos, :] = pos_gt_embed_weights.unsqueeze(-1)
-            embed_weights[:num_pos, :] *= pos_is_gt.unsqueeze(-1)
-            pos_weight = 1.0 if cfg.pos_weight <= 0 else cfg.pos_weight
-            label_weights[:num_pos] = pos_weight
-            if not self.reg_decoded_bbox:
-                pos_bbox_targets = self.bbox_coder.encode(
-                    pos_bboxes, pos_gt_bboxes)
-            else:
-                # When the regression loss (e.g. `IouLoss`, `GIouLoss`)
-                # is applied directly on the decoded bounding boxes, both
-                # the predicted boxes and regression targets should be with
-                # absolute coordinate format.
-                pos_bbox_targets = pos_gt_bboxes
-            bbox_targets[:num_pos, :] = pos_bbox_targets
-            bbox_weights[:num_pos, :] = 1
-        if num_neg > 0:
-            label_weights[-num_neg:] = 1.0
-
-        return labels, label_weights, bbox_targets, bbox_weights, \
-               embeds, embed_weights
-
-    def get_targets(self,
-                    sampling_results,
-                    gt_bboxes,
-                    gt_labels,
-                    gt_embeds,
-                    gt_embed_weights,
-                    rcnn_train_cfg,
-                    concat=True):
-        """
-
-        """
-        pos_bboxes_list = [res.pos_bboxes for res in sampling_results]
-        neg_bboxes_list = [res.neg_bboxes for res in sampling_results]
-        pos_gt_bboxes_list = [res.pos_gt_bboxes for res in sampling_results]
-        pos_gt_labels_list = [res.pos_gt_labels for res in sampling_results]
-        pos_gt_embeds_list = []
-        pos_gt_embed_weights_list = []
-        pos_is_gt_list = [res.pos_is_gt for res in sampling_results]
-        for i, res in enumerate(sampling_results):
-            pos_gt_embeds_list.append(gt_embeds[i][res.pos_assigned_gt_inds])
-            if gt_embed_weights is not None:
-                pos_gt_embed_weights_list.append(gt_embed_weights[i][res.pos_assigned_gt_inds])
-            else:
-                if self.use_img_loss:
-                    pos_gt_embed_weights_list.append(torch.ones_like(pos_gt_labels_list[i]))
-                else:
-                    pos_gt_embed_weights_list.append(torch.zeros_like(pos_gt_labels_list[i]))
-        labels, label_weights, bbox_targets, bbox_weights, \
-        embeds, embed_weights = multi_apply(
-            self._get_target_single,
-            pos_bboxes_list,
-            neg_bboxes_list,
-            pos_gt_bboxes_list,
-            pos_gt_labels_list,
-            pos_gt_embeds_list,
-            pos_gt_embed_weights_list,
-            pos_is_gt_list,
-            cfg=rcnn_train_cfg)
-
-        if concat:
-            labels = torch.cat(labels, 0)
-            label_weights = torch.cat(label_weights, 0)
-            bbox_targets = torch.cat(bbox_targets, 0)
-            bbox_weights = torch.cat(bbox_weights, 0)
-            embeds = torch.cat(embeds, 0)
-            embed_weights = torch.cat(embed_weights, 0)
-        return labels, label_weights, bbox_targets, bbox_weights, embeds, \
-               embed_weights
+        return region_embed
 
     @force_fp32(apply_to=('cls_score', 'bbox_pred'))
     def loss(self,
@@ -420,20 +293,17 @@ class EmbeddingBBoxHead(BBoxHead):
                 losses['loss_bbox'] = bbox_pred[pos_inds].sum()
 
         if region_embed is not None and self.use_img_loss:
-            # image loss condition is same with bounding box regression
-            bg_class_ind = self.num_classes
-            pos_inds = (labels >= 0) & (labels < bg_class_ind)
-            if pos_inds.any():
-                region_embed_pred = region_embed[pos_inds.type(torch.bool)]
-                losses['loss_img'] = self.loss_img(
-                    region_embed_pred,
-                    embeds[pos_inds.type(torch.bool)],
-                    embed_weights[pos_inds.type(torch.bool)],
-                    avg_factor=embeds.size(0),
-                    reduction_override=reduction_override
-                )
-            else:
-                losses['loss_img'] = region_embed[pos_inds].sum()
+            if embed_weights is not None:
+                embed_weights = embed_weights.unsqueeze(-1)
+                embed_weights = embed_weights.expand(-1, self.embed_channels)
+
+            losses['loss_img'] = self.loss_img(
+                region_embed,
+                embeds,
+                embed_weights,
+                avg_factor=embeds.size(0),
+                reduction_override=reduction_override
+            )
 
         return losses
     
@@ -460,69 +330,3 @@ class EmbeddingBBoxHead(BBoxHead):
                              f"but found {class_set}")
         
         self.num_classes = self.text_embeddings.shape[0]
-
-        
-    @force_fp32(apply_to=('cls_score', 'bbox_pred'))
-    def get_bboxes(self,
-                   rois,
-                   cls_score,
-                   bbox_pred,
-                   img_shape,
-                   scale_factor,
-                   rescale=False,
-                   cfg=None):
-        """Transform network output for a batch into bbox predictions.
-
-        Args:
-            rois (Tensor): Boxes to be transformed. Has shape (num_boxes, 5).
-                last dimension 5 arrange as (batch_index, x1, y1, x2, y2).
-            cls_score (Tensor): Box scores, has shape
-                (num_boxes, num_classes + 1).
-            bbox_pred (Tensor, optional): Box energies / deltas.
-                has shape (num_boxes, num_classes * 4).
-            img_shape (Sequence[int], optional): Maximum bounds for boxes,
-                specifies (H, W, C) or (H, W).
-            scale_factor (ndarray): Scale factor of the
-               image arrange as (w_scale, h_scale, w_scale, h_scale).
-            rescale (bool): If True, return boxes in original image space.
-                Default: False.
-            cfg (obj:`ConfigDict`): `test_cfg` of Bbox Head. Default: None
-
-        Returns:
-            tuple[Tensor, Tensor]:
-                First tensor is `det_bboxes`, has the shape
-                (num_boxes, 5) and last
-                dimension 5 represent (tl_x, tl_y, br_x, br_y, score).
-                Second tensor is the labels with shape (num_boxes, ).
-        """
-
-        # some loss (Seesaw loss..) may have custom activation
-        if self.custom_cls_channels:
-            scores = self.loss_cls.get_activation(cls_score)
-        else:
-            scores = F.softmax(
-                cls_score, dim=-1) if cls_score is not None else None
-        # bbox_pred would be None in some detector when with_reg is False,
-        # e.g. Grid R-CNN.
-        if bbox_pred is not None:
-            bboxes = self.bbox_coder.decode(
-                rois[..., 1:], bbox_pred, max_shape=img_shape)
-        else:
-            bboxes = rois[:, 1:].clone()
-            if img_shape is not None:
-                bboxes[:, [0, 2]].clamp_(min=0, max=img_shape[1])
-                bboxes[:, [1, 3]].clamp_(min=0, max=img_shape[0])
-
-        if rescale and bboxes.size(0) > 0:
-            scale_factor = bboxes.new_tensor(scale_factor)
-            bboxes = (bboxes.view(bboxes.size(0), -1, 4) / scale_factor).view(
-                bboxes.size()[0], -1)
-
-        if cfg is None:
-            return bboxes, scores
-        else:
-            det_bboxes, det_labels = multiclass_nms(bboxes, scores,
-                                                    cfg.score_thr, cfg.nms,
-                                                    cfg.max_per_img)
-
-            return det_bboxes, det_labels